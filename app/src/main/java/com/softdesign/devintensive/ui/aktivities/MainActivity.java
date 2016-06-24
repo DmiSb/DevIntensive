084069fa1a5b618b82fc9f7c758545d455b61301
DmiSb <DmiSb@inbox.ru>
2016-06-24 12:26:10 +0500

    Hometask_2 completed

--- app/src/main/java/com/softdesign/devintensive/ui/aktivities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/aktivities/MainActivity.java
@@ -2,14 +2,49 @@ package com.softdesign.devintensive.ui.aktivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
