272f41117857a7aa77b483b0166eec3b6faad49e
DmiSb <DmiSb@inbox.ru>
2016-07-07 21:58:07 +0500

    Hometask_4 correct edit text validator

--- app/src/main/java/com/softdesign/devintensive/utils/EditTextValidator.java
+++ app/src/main/java/com/softdesign/devintensive/utils/EditTextValidator.java
@@ -23,10 +23,12 @@ public class EditTextValidator implements TextWatcher {
     private TextInputLayout mEditLayout;
     // Тип элемента, константа
     private String mEditType;
-    // Позиция курсора
-    private int mPosStart, mPosEnd;
-    // Признак что курсор в конце текста
-    private Boolean mEndPosition = false;
+    // Позиция курсора до и после изменения
+    private int mPosBefore, mPosAfter;
+    // Значение до изменения
+    private String mValueBef = "";
+
+
 
     /**
      * Конструктор класса
@@ -48,7 +50,8 @@ public class EditTextValidator implements TextWatcher {
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        mPosBefore = mEdit.getSelectionStart();
+        mValueBef = mEdit.getText().toString();
     }
 
     @Override
@@ -59,20 +62,23 @@ public class EditTextValidator implements TextWatcher {
     /**
      * Событие после изменения текста
      *
-     * @param s
+     * @param s - текст
      */
     @Override
     public void afterTextChanged(Editable s) {
 
+        Boolean endPosition = false;
+
         mEdit.removeTextChangedListener(this);
-        mPosStart = mEdit.getSelectionStart();
-        mPosEnd = mEdit.getSelectionEnd();
-        if (mPosStart == mEdit.getText().length())
-            mEndPosition = true;
+        mPosAfter = mEdit.getSelectionStart();
+
+        if (mPosAfter == mEdit.getText().length()) {
+            endPosition = true;
+        }
 
         switch (mEditType) {
             case ConstantManager.USER_PHONE_KEY:
-                mEdit.setText(validatePhone(s.toString()));
+                mEdit.setText(validatePhone(s.toString(), endPosition));
                 break;
             case ConstantManager.USER_EMAIL_KEY:
                 validateEmail(s.toString());
@@ -85,14 +91,10 @@ public class EditTextValidator implements TextWatcher {
         }
 
         mEdit.addTextChangedListener(this);
-        if (mEndPosition) {
+        if (endPosition) {
             mEdit.setSelection(mEdit.getText().length());
         } else {
-            if (mPosStart == mPosEnd) {
-                mEdit.setSelection(mPosStart);
-            } else {
-                mEdit.setSelection(mPosStart, mPosEnd);
-            }
+            mEdit.setSelection(mPosAfter);
         }
     }
 
@@ -118,7 +120,7 @@ public class EditTextValidator implements TextWatcher {
      * Проветка на длину номера телефона
      * При длине менее 11 символов выводит ошибку
      *
-     * @param phone
+     * @param phone - телефонный номер
      */
     private void checkPhoneLength(String phone) {
         if (phone.length() < 11) {
@@ -133,24 +135,27 @@ public class EditTextValidator implements TextWatcher {
      * Форматирование в требуемом формате
      * При длине менее 11 символов выводит ошибку
      *
-     * @param phone
-     * @return
+     * @param phone - телефонный номер
+     * @return - форматированный телефонный номер
      */
-    private String validatePhone(String phone) {
+    private String validatePhone(String phone, Boolean endPosition) {
 
         String oldVal = "";
         String newVal = "";
         int i;
+        int dropSymbol = -1;
+
+        if ((mPosBefore > mPosAfter) && (!endPosition)) {
+            if (! Character.isDigit(mValueBef.charAt(mPosAfter))) {
+                mPosAfter--;
+                dropSymbol = mPosAfter;
+            }
+        }
 
         if (phone != null && phone.length() > 0) {
             for (i = 0; i < phone.length(); i++) {
-                if (Character.isDigit(phone.charAt(i))) {
+                if ((Character.isDigit(phone.charAt(i))) && (i != dropSymbol)) {
                     oldVal = oldVal + phone.charAt(i);
-                } else {
-                    if (i <= mPosStart)
-                        mPosStart--;
-                    if (i <= mPosEnd)
-                        mPosEnd--;
                 }
             }
 
@@ -168,28 +173,16 @@ public class EditTextValidator implements TextWatcher {
                     switch (i) {
                         case 0:
                             newVal = "+" + oldVal.charAt(i);
-                            if (i < mPosStart)
-                                mPosStart++;
-                            if (i < mPosEnd)
-                                mPosEnd++;
                             l++;
                             break;
                         case 1:
                         case 4:
                             newVal = newVal + " " + oldVal.charAt(i);
-                            if (i < mPosStart)
-                                mPosStart++;
-                            if (i < mPosEnd)
-                                mPosEnd++;
                             l++;
                             break;
                         case 7:
                         case 9:
                             newVal = newVal + "-" + oldVal.charAt(i);
-                            if (i < mPosStart)
-                                mPosStart++;
-                            if (i < mPosEnd)
-                                mPosEnd++;
                             l++;
                             break;
                         default:
@@ -197,16 +190,17 @@ public class EditTextValidator implements TextWatcher {
                     }
                 }
 
-                if (mPosStart < 0)
-                    mPosStart = 0;
-                if (mPosEnd < 0)
-                    mPosEnd = 0;
+                if (mPosAfter < 0)
+                    mPosAfter = 0;
 
                 if (l > 20) {
                     l = 20;
                 }
 
-                Log.d(TAG, oldVal + ", " + newVal + ", " + Integer.toString(mPosStart) + ", " + Integer.toString(mPosEnd));
+                Log.d(TAG, "oldValue=" + oldVal + ", newValue=" + newVal +
+                        ", PosBefore=" + Integer.toString(mPosBefore) +
+                        ", mPosAfter=" + Integer.toString(mPosAfter) +
+                        ", dropSymbol=" + Integer.toString(dropSymbol));
                 checkPhoneLength(newVal);
                 return newVal.substring(0, l);
             } else {
@@ -225,7 +219,7 @@ public class EditTextValidator implements TextWatcher {
      * Минимальное число перед @ - 3, перед точкой - 2, домен - 2
      * Выводит ошибку
      *
-     * @param email
+     * @param email - почтовый ящик
      */
     private void validateEmail(String email) {
 
@@ -254,8 +248,7 @@ public class EditTextValidator implements TextWatcher {
     private String validateLink(String link, String domain) {
 
         String newValue = link;
-        int s = link.length() - mPosStart;
-        int e = link.length() - mPosEnd;
+        int s = link.length() - mPosAfter;
 
         int posVK = link.toLowerCase().indexOf(domain);
         if (posVK >= 0) {
@@ -264,8 +257,7 @@ public class EditTextValidator implements TextWatcher {
         } else {
             showError(R.string.link_error_value);
         }
-        mPosStart = newValue.length() - s;
-        mPosEnd = newValue.length() - e;
+        mPosAfter = newValue.length() - s;
 
         return  newValue;
     }
