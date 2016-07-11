9a50b2ea3a674d34b562d9e5f2fa34997c9ed333
DmiSb <DmiSb@inbox.ru>
2016-07-11 22:10:59 +0500

    Merge branch 'master' of D:\Android\projects\DevIntensive with conflicts.

--- app/src/main/java/com/softdesign/devintensive/data/managers/PreferencesManager.java
+++ app/src/main/java/com/softdesign/devintensive/data/managers/PreferencesManager.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.data.managers;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
@@ -9,19 +10,35 @@ import com.softdesign.devintensive.utils.DevIntensiveApp;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс сохранения и получения данных
+ */
 public class PreferencesManager {
+
     private SharedPreferences mSharedPreferences;
-    public static final String[] USER_FIELDS = {
+
+    private static final String[] USER_FIELDS = {
             ConstantManager.USER_PHONE_KEY,
             ConstantManager.USER_EMAIL_KEY,
             ConstantManager.USER_VK_KEY,
             ConstantManager.USER_GIT_KEY,
-            ConstantManager.USER_SELF_KEY};
+            ConstantManager.USER_SELF_KEY
+    };
+    private static final String[] USER_RATINGS = {
+            ConstantManager.USER_RATING_KEY,
+            ConstantManager.USER_CODE_LINE_KEY,
+            ConstantManager.USER_PROJECT_KEY
+    };
 
     public PreferencesManager() {
         mSharedPreferences = DevIntensiveApp.getSharedPreferences();
     }
 
+    /**
+     * Сохранение данных
+     *
+     * @param userFields
+     */
     public void saveUserProfileData (List<String> userFields){
         SharedPreferences.Editor editor = mSharedPreferences.edit();
 
@@ -31,17 +48,80 @@ public class PreferencesManager {
         editor.apply();
     }
 
+    /**
+     * Загрузка данных
+     *
+     * @return
+     */
     public List<String> loadUserProfileData (){
         List<String> userFields = new ArrayList<>();
-        List<String> defaultUserData = new ArrayList<>();
-        defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.phone_value));
-        defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.email_value));
-        defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.vk_value));
-        defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.git_value));
-        defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.self_value));
+        //List<String> defaultUserData = new ArrayList<>();
+        //defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.phone_value));
+        //defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.email_value));
+        //defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.vk_value));
+        //defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.git_value));
+        //defaultUserData.add(DevIntensiveApp.getContext().getResources().getString(R.string.self_value));
         for (int i = 0; i < USER_FIELDS.length; i++) {
-            userFields.add(mSharedPreferences.getString(USER_FIELDS[i], defaultUserData.get(i)));
+            userFields.add(mSharedPreferences.getString(USER_FIELDS[i], ""/*defaultUserData.get(i)*/));
         }
         return userFields;
+    }
+
+    /**
+     * Сохранение фотографии профиля
+     *
+     * @param uri
+     */
+    public void saveUserPhoto(Uri uri) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_PHOTO_KEY, uri.toString());
+        editor.apply();
+    }
+
+    /**
+     * Получение фотографии профиля
+     *
+     * @return
+     */
+    public Uri loadUserPhoto() {
+        return Uri.parse(mSharedPreferences.getString(ConstantManager.USER_PHOTO_KEY, ""));
+                //"android.resource://com.softdesign.devintensive/drawable/user_photo"));
+    }
+
+    public void saveAuthToken(String authToken) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.AUTH_TOKEN_KEY, authToken);
+        editor.apply();
+    }
+
+    public String getAuthToken() {
+        return mSharedPreferences.getString(ConstantManager.AUTH_TOKEN_KEY, "null");
+    }
+
+    public void saveUserId(String userId) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_ID_KEY, userId);
+        editor.apply();
+    }
+
+    public String getUserId() {
+        return mSharedPreferences.getString(ConstantManager.USER_ID_KEY, "null");
+    }
+
+    public void saveUserRatingValues(int[] userRatings) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i <USER_RATINGS.length; i++){
+            editor.putString(USER_RATINGS[i], String.valueOf(userRatings[i]));
+        }
+        editor.apply();
+    }
+
+    public List<String> loadUserRatingValues() {
+        List<String> userRatings = new ArrayList<>();
+        for (int i = 0; i < USER_RATINGS.length; i++) {
+            userRatings.add(mSharedPreferences.getString(USER_RATINGS[i], "0"));
+        }
+        return userRatings;
     }
 }
